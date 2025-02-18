34afac1fae5614cd98bcd5586c90cd166371bc6b
kab08737 <kaylee.bae@kub.org>
2025-02-18 07:46:47 -0500

    pg 155 working

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -3,5 +3,13 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.modelFor('bands.band');
+  },
+
+  resetController(controller) {
+    controller.setProperties({
+      isAddingSong: false,
+      newSongTitle: ''
+    })
+
   }
 })
