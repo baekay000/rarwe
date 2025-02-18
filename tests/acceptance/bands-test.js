ac79c187e38aea5439da1672b9f6574ed3fe734e
kab08737 <kaylee.bae@kub.org>
2025-02-18 12:40:51 -0500

    p 179

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,13 +1,22 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Acceptance | bands', function(hooks) {
+
+module('Acceptance | Bands', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
-  test('visiting /bands', async function(assert) {
-    await visit('/bands');
+  test('List bands', async function(assert) {
+    this.server.create('band', { name: 'Radiohead' });
+    this.server.create('band', { name: 'Long Distance Calling' });
 
-    assert.equal(currentURL(), '/bands');
+    await visit('/');
+    let bandLinks = document.querySelectorAll('.rr-band-link');
+    assert.equal(bandLinks.length, 2, 'All band links are rendered');
+    assert.ok(bandLinks[0].textContent.includes('Radiohead'), 'First band link contains the band name');
+    assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'), 'The other band link contains the band name');
+
   });
 });
